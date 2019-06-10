9c34d7c398f7c9bbc22491b2ecbf5d8554b87d34
Daniele Rizzo <calupietru@gmail.com>
2019-06-10 10:28:19 +0200

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,6 +1,7 @@
 import uos
 import machine
 import network
+import time
 
 def mount_sd():
     uos.sdconfig(uos.SDMODE_SPI, clk=18, mosi=23, miso=19, cs=4)
@@ -10,8 +11,9 @@ def clear_ledbar():
     ledbar = machine.Neopixel(machine.Pin(15), 10)
     ledbar.clear()
 
-def connect(ssid,password):
+def connect_wifi(ssid,password):
     n = network.WLAN(network.STA_IF)
     n.active(True)
     n.connect(ssid,password)
+    time.sleep(5000)
     return n.ifconfig()
